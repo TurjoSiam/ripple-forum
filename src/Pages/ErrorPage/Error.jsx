699bc38c3f0b3fa98cab5748d1c55e61c4676db5
TurjoSiam <t.siam24@gmail.com>
2025-01-18 13:06:40 +0600

    react router, tailwind, daisy ui installed and basic layout added

---
+++ src/Pages/ErrorPage/Error.jsx
@@ -0,0 +1,11 @@
+import error from "../../assets/error.jpg"
+
+const Error = () => {
+    return (
+        <div className="w-full flex justify-center my-12">
+            <img src={error} alt="error page" />
+        </div>
+    );
+};
+
+export default Error;
