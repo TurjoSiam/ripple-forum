eade95d91d0c677d14defb46295077f379708039
TurjoSiam <t.siam24@gmail.com>
2025-01-27 11:53:02 +0600

    announcement page and form added

--- src/Pages/Dashboard/Outlets/Announcement.jsx
+++ src/Pages/Dashboard/Outlets/Announcement.jsx
@@ -1,11 +1,58 @@
+import { useContext } from "react";
+import AuthContext from "../../../Context/AuthContext";
+import useAxiosPrivate from "../../../Hooks/useAxiosPrivate";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 
 const Announcement = () => {
+
+    const { user } = useContext(AuthContext);
+
+    const time = new Date().toJSON().slice(11, 16);
+
+    const { register, handleSubmit, reset } = useForm();
+
+    const axiosPrivate = useAxiosPrivate();
+
+    const onSubmit = async (data) => {
+        const res = await axiosPrivate.post('/announcements', data)
+        if (res.data.insertedId) {
+            Swal.fire("Announcement Added Successfully!");
+        }
+        reset();
+    }
+
+
+
+
+
     return (
-        <div className="col-span-9 text-center">
-            <h2>this is announcement page</h2>
+        <div className="lg:my-10 col-span-9 w-5/6 mx-auto">
+            <form onSubmit={handleSubmit(onSubmit)} className="mx-8 bg-orange-50 p-16 rounded-3xl border border-cyan-200">
+                <div className="relative z-0 w-full mb-5 group">
+                    <input type="text" {...register('authorName')} defaultValue={user?.displayName} readOnly className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Author Name</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input {...register('authorImage')} defaultValue={user?.photoURL} readOnly type="url" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Author Image</label>
+                </div>
+
+                <div className="relative z-0 w-full mb-5 group">
+                    <input type="text" {...register('title')} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Post Title</label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <textarea type="text" {...register('description')} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Description</label>
+                </div>
+                <input {...register('time')} hidden defaultValue={time} type="text" name="" id="" />
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Announcement</button>
+            </form>
         </div>
     );
-};
+
+}
 
 export default Announcement;
