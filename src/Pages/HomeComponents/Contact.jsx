43c720d468d3f07d1353504bb4d18770d1b75c9e
TurjoSiam <t.siam24@gmail.com>
2025-04-26 20:55:24 +0600

    [ADDED]-contact us form email sending functionality added

--- src/Pages/HomeComponents/Contact.jsx
+++ src/Pages/HomeComponents/Contact.jsx
@@ -1,10 +1,40 @@
+import { Slide, toast } from "react-toastify";
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "7fb4ed41-bc76-4258-a62f-fb0b15721048");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            toast.success("Message sent successfully!", {
+                position: "bottom-right",
+                transition: Slide
+            });
+        }
+    };
+
+
+
     return (
         <div className="w-full my-16 mx-auto">
             <h2 className="text-2xl font-bold text-center">Contact Us</h2>
-            <form className="card-body lg:w-8/12 w-10/12 mx-auto">
-            {/* name field */}
+            <form onSubmit={onSubmit} className="card-body lg:w-8/12 w-10/12 mx-auto">
+                {/* name field */}
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Name</span>
