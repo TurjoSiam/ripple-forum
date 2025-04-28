2e567f2e13aadb622afe1c319cac83874e4daf56
TurjoSiam <t.siam24@gmail.com>
2025-04-28 21:51:09 +0600

    [ADDED]-comment reporting componenet added to comment table

--- src/Pages/Comments/Comments.jsx
+++ src/Pages/Comments/Comments.jsx
@@ -1,25 +1,11 @@
 
-import { useForm } from "react-hook-form";
 import { useLoaderData } from "react-router-dom";
-import Swal from "sweetalert2";
-import useAxiosPrivate from "../../Hooks/useAxiosPrivate";
+
+import ReportedComments from "../../Components/ReportedComments";
 
 const Comments = () => {
 
     const postComments = useLoaderData();
-
-    const { register, handleSubmit, reset } = useForm();
-
-    const axiosPrivate = useAxiosPrivate();
-    const onSubmit = async (data) => {
-        const res = await axiosPrivate.post('/reports', data)
-        if (res.data.insertedId) {
-            Swal.fire("Report Added Successfully!");
-        }
-        reset();
-    }
-
-
 
     return (
         <div className="w-11/12 md:w-9/12 mx-auto my-10 bg-orange-50 rounded-2xl border border-orange-300 p-7">
@@ -41,17 +27,7 @@ const Comments = () => {
                                 <td>{comment?.email}</td>
                                 <td>{comment?.comment}</td>
                                 <td>
-                                    <form onSubmit={handleSubmit(onSubmit)}>
-                                        <select required {...register('report')} className="select mr-1">
-                                            <option value="Offensive Language">Offensive Language</option>
-                                            <option value="Spam Comment">Spam Comment</option>
-                                            <option value="Bullying Behavior">Bullying Behavior</option>
-                                        </select>
-                                        <input defaultValue={comment.email} hidden {...register('email')} type="text" />
-                                        <input defaultValue={comment.comment} hidden {...register('comment')} type="text" />
-                                        <input defaultValue={comment._id} hidden {...register('commentId')} type="text" />
-                                        <input type="submit" value="Report" className="btn bg-red-300 hover:bg-red-400" />
-                                    </form>
+                                    <ReportedComments comments={comment}></ReportedComments>
                                 </td>
                             </tr>)
                         }
