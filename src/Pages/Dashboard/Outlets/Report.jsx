0ed75d86683f9810e2bab961eddaf915f19c7331
TurjoSiam <t.siam24@gmail.com>
2025-04-28 22:03:06 +0600

    [ADDED]-reported comments table head added

--- src/Pages/Dashboard/Outlets/Report.jsx
+++ src/Pages/Dashboard/Outlets/Report.jsx
@@ -16,12 +16,14 @@ const Report = () => {
 
     return (
         <div className="col-span-9 text-center bg-orange-50 p-10 w-4/6 my-10 mx-auto">
-            <h1 className="text-3xl font-bold text-center">Reported Comments</h1>
+            <h1 className="text-3xl font-bold text-center mb-8">Reported Comments</h1>
             <table>
                 <thead>
                     <tr>
                         <th>Serial</th>
                         <th>Comment</th>
+                        <th>Commenter Email</th>
+                        <th><button className="btn btn-error">Delete</button></th>
                     </tr>
                 </thead>
             </table>
