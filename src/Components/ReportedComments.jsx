f001e9528fd8483eee02fbbe13d810d8cd02b622
TurjoSiam <t.siam24@gmail.com>
2025-04-28 21:46:54 +0600

    [ADDED]-design and functionality added in comment reporting form

--- src/Components/ReportedComments.jsx
+++ src/Components/ReportedComments.jsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import useAxiosPrivate from "../Hooks/useAxiosPrivate";
 import Swal from "sweetalert2";
 
-const ReportedComments = ({comments}) => {
+const ReportedComments = ({ comments }) => {
 
     const { register, handleSubmit, reset } = useForm();
 
@@ -18,7 +18,17 @@ const ReportedComments = ({comments}) => {
 
     return (
         <div>
-            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <select required {...register('report')} className="select mr-1">
+                    <option value="Offensive Language">Offensive Language</option>
+                    <option value="Spam Comment">Spam Comment</option>
+                    <option value="Bullying Behavior">Bullying Behavior</option>
+                </select>
+                <input defaultValue={comments.email} hidden {...register('email')} type="text" />
+                <input defaultValue={comments.comment} hidden {...register('comment')} type="text" />
+                <input defaultValue={comments._id} hidden {...register('commentId')} type="text" />
+                <input type="submit" value="Report" className="btn bg-red-300 hover:bg-red-400" />
+            </form>
         </div>
     );
 };
