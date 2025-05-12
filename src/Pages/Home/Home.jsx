19bfa92b55481da65e68d0d03b8cbdefc0e6d485
TurjoSiam <t.siam24@gmail.com>
2025-05-12 23:29:45 +0600

    [MODIFIED]-home page modified with banner delete and grid view

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import Accordion from "../HomeComponents/Accordion";
-import Banner from "../HomeComponents/Banner";
 import Contact from "../HomeComponents/Contact";
 import Countdown from "../HomeComponents/Countdown";
 import Posts from "../HomeComponents/Posts";
@@ -11,14 +10,22 @@ import Timeline from "../HomeComponents/Timeline";
 const Home = () => {
     return (
         <div className="bg-blue-100 dark:bg-gray-600">
-            <Banner></Banner>
-            <Posts></Posts>
-            <ShowAnnouncement></ShowAnnouncement>
-            <Timeline></Timeline>
-            <Steps></Steps>
-            <Accordion></Accordion>
-            <Countdown></Countdown>
-            <Contact></Contact>
+            <div className="grid grid-cols-2 gap-5">
+                {/* left side options */}
+                <div>
+                    <h2>this is left side options</h2>
+                </div>
+                {/* right side options */}
+                <div>
+                    <Posts></Posts>
+                    <ShowAnnouncement></ShowAnnouncement>
+                    <Timeline></Timeline>
+                    <Steps></Steps>
+                    <Accordion></Accordion>
+                    <Countdown></Countdown>
+                    <Contact></Contact>
+                </div>
+            </div>
         </div>
     );
 };
