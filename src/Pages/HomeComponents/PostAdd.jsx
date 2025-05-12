6bd2a2225ca5694dfbfaa3c326644d2c741a925f
TurjoSiam <t.siam24@gmail.com>
2025-05-12 23:49:33 +0600

    [ADDED]-new post option added

---
+++ src/Pages/HomeComponents/PostAdd.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PostAdd = () => {
+    return (
+        <div>
+            <div>
+                <img className='w-5 h-5 rounded-full' src="" alt="Profile picture" />
+                <textarea className='w-full rounded-lg min-h-10' name="post" id="post"></textarea>
+            </div>
+        </div>
+    );
+};
+
+export default PostAdd;
