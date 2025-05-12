5f6f369433fb1832b926ddaaf09bd6630ee6c15f
TurjoSiam <t.siam24@gmail.com>
2025-05-13 00:32:25 +0600

    [ADDED]-privacy options added for post

--- src/Pages/HomeComponents/PostAdd.jsx
+++ src/Pages/HomeComponents/PostAdd.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 const PostAdd = () => {
     return (
         <div>
-            <div>
+            <div className='flex items-start gap-3'>
                 <img className='w-5 h-5 rounded-full' src="" alt="Profile picture" />
                 <textarea className='w-full rounded-lg min-h-10' name="post" id="post"></textarea>
+            </div>
+            <div>
+                <div>
+                    <input name='privacy' type="text" />
+                    <datalist id='privacy'>
+                        <option value="Public">Public</option>
+                        <option value="Friends">Friends</option>
+                        <option value="Only me">Only me</option>
+                    </datalist>
+                </div>
             </div>
         </div>
     );
