4fbf9e44b5ed715441a2e0b2b66e1857032bf9aa
TurjoSiam <t.siam24@gmail.com>
2025-05-13 23:38:18 +0600

    [UPDATED]-some styling updated

--- src/Pages/HomeComponents/PostAdd.jsx
+++ src/Pages/HomeComponents/PostAdd.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PostAdd = () => {
     return (
-        <div>
+        <div className='mt-10 max-w-2xl mx-auto'>
             <div className='flex items-start gap-3'>
                 <img className='w-5 h-5 rounded-full' src="" alt="Profile picture" />
                 <textarea className='w-full rounded-lg min-h-10' name="post" id="post"></textarea>
             </div>
-            <div>
+            <div className='flex items-center mt-5 justify-between'>
                 <div>
                     <input name='privacy' type="text" />
                     <datalist id='privacy'>
