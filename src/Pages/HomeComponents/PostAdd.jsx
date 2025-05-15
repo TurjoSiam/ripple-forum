c5be4127766a6633577f716616c1a013a2cc2488
TurjoSiam <t.siam24@gmail.com>
2025-05-15 22:20:55 +0600

    [UPDATED]-posting modified using form

--- src/Pages/HomeComponents/PostAdd.jsx
+++ src/Pages/HomeComponents/PostAdd.jsx
@@ -4,8 +4,6 @@ import AuthContext from "../../Context/AuthContext";
 
 const PostAdd = () => {
 
-    const { user } = useContext(AuthContext);
-
     const time = new Date().toJSON().slice(11, 16);
 
     const { register, handleSubmit, reset } = useForm();
@@ -18,21 +16,23 @@ const PostAdd = () => {
 
     return (
         <div className='mt-10 max-w-2xl mx-auto'>
-            <div className='flex items-start gap-3'>
-                <img className='w-12 h-12 object-cover rounded-full' src={user?.photoURL} alt="Profile picture" />
-                <textarea placeholder='Share your thoughts !' className='p-5 w-full shadow-md rounded-lg min-h-20' name="post" id="post"></textarea>
-            </div>
-            <div className='flex items-center mt-2 justify-between'>
-                <div className="ml-16 space-x-1">
-                    <label className="text-sm font-semibold" htmlFor="privacy">Privacy:</label>
-                    <select className="text-sm rounded-md" name="privacy" id="privacy">
-                        <option value="Public">Public</option>
-                        <option value="Friends">Friends</option>
-                        <option value="Only me">Only me</option>
-                    </select>
+            <form>
+                <div className='flex items-start gap-3'>
+                    <img className='w-12 h-12 object-cover rounded-full' src={user?.photoURL} alt="Profile picture" />
+                    <textarea {...register('post')} placeholder='Share your thoughts !' className='p-5 w-full shadow-md rounded-lg min-h-20' name="post" id="post"></textarea>
                 </div>
-                <button onClick={handleClick} className='btn' type='submit'>Post</button>
-            </div>
+                <div className='flex items-center mt-2 justify-between'>
+                    <div className="ml-16 space-x-1">
+                        <label className="text-sm font-semibold" htmlFor="privacy">Privacy:</label>
+                        <select {...register('privacy')} className="text-sm rounded-md" name="privacy" id="privacy">
+                            <option value="Public">Public</option>
+                            <option value="Friends">Friends</option>
+                            <option value="Only me">Only me</option>
+                        </select>
+                    </div>
+                    <button onClick={handleClick} className='btn' type='submit'>Post</button>
+                </div>
+            </form>
         </div>
     );
 };
